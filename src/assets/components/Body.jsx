2895eabd2041a248e11bcfd5120cb748bfba618b
ZhugeB1ao <doanhuynhgiabao0508@gmail.com>
2025-03-25 18:32:25 +0700

    Bao commit 18:32 25/3/2025

--- src/assets/components/Body.jsx
+++ src/assets/components/Body.jsx
@@ -1,25 +1,156 @@
-const Body = ({ movies }) => {
-    return (
-        <div className="grid grid-cols-4 gap-4 p-8 bg-[#141414]">
-            {movies.slice(1).map((movie) => (
-                <div key={movie.id} className="p-4">
-                    {/*<img*/}
-                    {/*    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}*/}
-                    {/*    alt={movie.title}*/}
-                    {/*    className="rounded-lg"*/}
-                    {/*/>*/}
+import React, { useState, useRef, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation } from "swiper/modules";
+import { Pagination } from "swiper/modules";
 
-                    <div
-                        className="w-full h-48 bg-cover bg-center rounded-lg relative "
-                        style={{backgroundImage: `url(https://image.tmdb.org/t/p/w500/${movie.backdrop_path})`}}
-                    >
-                        <h3 className="text-white m-2 text-lg font-semibold absolute">{movie.title}</h3>
-                    </div>
-
-                </div>
+const Section = ({ title, movies }) => {
+  return (
+    <Swiper
+      slidesPerView={4}
+      slidesPerGroup={1}
+      loop={true}
+      navigation={true}
+      pagination={{ clickable: true }}
+      modules={[Pagination, Navigation]}
+      className="relative bg-[#141414] text-white h-40"
+    >
+      {/* {movies.map((group, index) => (
+        <SwiperSlide key={index} className="p-4">
+          <div className="flex gap-4">
+            {group.map((movie) => (
+              <div
+                key={movie.id}
+                className="w-1/5 h-48 bg-cover bg-center rounded-lg relative transition-transform duration-300 hover:scale-105"
+                style={{
+                  backgroundImage: `url(https://image.tmdb.org/t/p/w500/${movie.backdrop_path})`,
+                }}
+              >
+                <h3 className="text-white text-lg font-semibold absolute bottom-0 left-1 text">
+                  {movie.title || movie.name}
+                </h3>
+              </div>
             ))}
-        </div>
-    );
+          </div>
+        </SwiperSlide>
+      ))} */}
+      <SwiperSlide>111</SwiperSlide>
+    </Swiper>
+  );
+};
+
+const Body = (moviesData) => {
+    console.log(moviesData)
+  return (
+    <div>
+      {Object.entries(moviesData).map(([category, movies]) => (
+        <Section key={category} title={category} movies={movies} />
+      ))}
+    </div>
+  );
 };
 
 export default Body;
+//
+// const chunkArrayWithOverlap = (arr, size) => {
+//     let result = [];
+//     for (let i = 0; i < arr.length; i += size) {
+//         let chunk = arr.slice(i, i + size);
+//         if (i + size >= arr.length && chunk.length < size) {
+//             let prevChunk = result[result.length - 1];
+//             if (prevChunk) {
+//                 let lastItem = prevChunk[prevChunk.length - 1];
+//                 chunk.unshift(lastItem);
+//             }
+//         }
+//         result.push(chunk);
+//     }
+//     return result;
+// };
+//
+// const Section = ({ title, movies }) => {
+//     const movieGroups = chunkArrayWithOverlap(movies, 5);
+//     const swiperRef = useRef(null);
+//     const [showPrev, setShowPrev] = useState(false);
+//
+//     const handleNext = () => {
+//         if (swiperRef.current) {
+//             swiperRef.current.slideNext();
+//             setShowPrev(true);
+//         }
+//     };
+//
+//     const handlePrev = () => {
+//         if (swiperRef.current) {
+//             swiperRef.current.slidePrev();
+//             if (swiperRef.current.realIndex === 0) setShowPrev(false);
+//         }
+//     };
+//
+//     return (
+//         <div className="relative">
+//             <h2 className="bg-[#141414] text-white px-12">{title}</h2>
+//             <div className="flex items-center relative bg-[#141414]">
+//                 {showPrev && (
+//                     <button
+//                         className="absolute left-0 z-10 bg-black bg-opacity-50 text-white px-4 py-2 rounded transition-transform duration-300 hover:scale-110"
+//                         onClick={handlePrev}
+//                     >
+//                         ◀
+//                     </button>
+//                 )}
+//                 <div className="w-full overflow-hidden">
+//                     <Swiper
+//                         modules={[Navigation]}
+//                         spaceBetween={10}
+//                         slidesPerView={1}
+//                         slidesPerGroup={1}
+//                         loop={true}
+//                         allowTouchMove={false}
+//                         onSwiper={(swiper) => (swiperRef.current = swiper)}
+//                     >
+//                         {movieGroups.map((group, index) => (
+//                             <SwiperSlide key={index} className="p-4">
+//                                 <div className="flex gap-4">
+//                                     {group.map((movie) => (
+//                                         <div
+//                                             key={movie.id}
+//                                             className="w-1/5 h-48 bg-cover bg-center rounded-lg relative transition-transform duration-300 hover:scale-105"
+//                                             style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w500/${movie.backdrop_path})` }}
+//                                         >
+//                                             <h3 className="text-white text-lg font-semibold absolute bottom-0 left-1 text">
+//                                                 {movie.title || movie.name}
+//                                             </h3>
+//                                         </div>
+//                                     ))}
+//                                 </div>
+//                             </SwiperSlide>
+//                         ))}
+//                     </Swiper>
+//                 </div>
+//                 {movieGroups.length > 1 && (
+//                     <button
+//                         className="absolute right-0 z-10 bg-black bg-opacity-50 text-white px-4 py-2 rounded transition-transform duration-300 hover:scale-110"
+//                         onClick={handleNext}
+//                     >
+//                         ▶
+//                     </button>
+//                 )}
+//             </div>
+//         </div>
+//     );
+// };
+//
+// const Body = (moviesData ) => {
+//     return (
+//         <div>
+//             {Object.entries(moviesData).map(([category, movies]) => (
+//                 <Section key={category} title={category} movies={movies} />
+//             ))}
+//         </div>
+//     );
+// };
+//
+// export default Body;
--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -4,13 +4,13 @@ const Header = ({movie}) => {
     return (
         <header
             className="flex justify-start items-center px-28 py-40 m-0 text-white h-[90vh] w-full relative"
-            style={{backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`}}
+            style={{background: `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path}) center/cover no-repeat`}}
         >
             <div className="absolute inset-0 bg-gradient-to-b from-transparent to-[#141414] h-full "></div>
             <div className="absolute inset-0 bg-gradient-to-l from-transparent to-[#141414] h-full "></div>
 
-            <div className="max-w-2xl relative z-10">
-                <h1 className="text-center text-7xl font-bold">{movie?.title || "Unknown Title"}</h1>
+            <div className="max-w-xl relative z-10 flex  flex-col">
+                <h1 className="text-center text-5xl font-bold">{movie?.title || "Unknown Title"}</h1>
                 <ul className="flex justify-center space-x-4 my-5">
                     <li className="border px-2 py-1 rounded">{movie?.adult ? "18+" : "13+"}</li>
                     <li className="px-2 py-[5px]">{movie?.release_date ? movie.release_date.split("-")[0] : "N/A"}</li>
@@ -22,7 +22,8 @@ const Header = ({movie}) => {
                         }
                     </li>
                 </ul>
-                <p className="text-left text-xl mt-4">{movie?.overview || "No description available"}</p>
+                <p className="text-left text-lg mt-4">{movie?.overview || "No description available"}</p>
+                <button className="my-10 mx-24 p-5 border-2 rounded-3xl text-2xl ">Watch now</button>
             </div>
         </header>
     );
