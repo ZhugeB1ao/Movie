32d00eed83ec7de17f3fadd4e9976b461801936e
ZhugeB1ao <doanhuynhgiabao0508@gmail.com>
2025-04-15 18:11:18 +0700

    Bao commit 18:11 15/4/2025

--- src/assets/components/Body.jsx
+++ src/assets/components/Body.jsx
@@ -5,49 +5,75 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Navigation, Pagination } from "swiper/modules";
 import MovieCard from "./MovieCard.jsx";
+import { useState } from "react";
 
 const Section = ({ title, movies, index }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  // eslint-disable-next-line no-unused-vars
+  let swiperInstance = null;
+
   return (
-    <div className="relative">
+    <div className="py-10 h-25vh">
       <h1 className="pb-4 pl-10 text-xl">{title}</h1>
+      <div className="relative w-full h-44">
+        <button
+          className={`custom-prev-${index} absolute left-0 top-0 h-[100%] transform bg-black text-white p-2 z-10 rounded-tr-md rounded-br-md`}
+        >
+          <ChevronLeft size={24} />
+        </button>
+        <button
+          className={`custom-next-${index} absolute right-0 top-0 h-[100%] transform bg-black text-white p-2 z-10 rounded-tl-md rounded-bl-md`}
+        >
+          <ChevronRight size={24} />
+        </button>
 
-      <button
-        className={`custom-prev-${index} absolute -left-8 top-1/2 transform bg-black text-white p-2 z-10 rounded-full`}
-      >
-        <ChevronLeft size={24} />
-      </button>
-      <button
-        className={`custom-next-${index} absolute -right-8 top-1/2 transform bg-black text-white p-2 z-10 rounded-full`}
-      >
-        <ChevronRight size={24} />
-      </button>
+        <div
+          className="absolute top-0 left-[50px] right-[50px] h-full overflow-y-visible"
+          style={{ width: "calc(100% - 100px)" }}
+        >
+          <Swiper
+            slidesPerView={6}
+            spaceBetween={10}
+            slidesPerGroup={1}
+            loop
+            onSwiper={(swiper) => {
+              swiperInstance = swiper;
+            }}
+            onSlideChange={(swiper) => {
+              setActiveIndex(swiper.realIndex);
+            }}
+            navigation={{
+              nextEl: `.custom-next-${index}`,
+              prevEl: `.custom-prev-${index}`,
+            }}
+            pagination={{ clickable: true }}
+            modules={[Pagination, Navigation]}
+            className="w-full overflow-visible"
+          >
+            {movies.map((movie) => (
+              <SwiperSlide key={movie.id} className="relative overflow-visible">
+                <MovieCard movie={movie} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
 
-      <Swiper
-        slidesPerView={6}
-        slidesPerGroup={1}
-        loop
-        navigation={{
-          nextEl: `.custom-next-${index}`,
-          prevEl: `.custom-prev-${index}`,
-        }}
-        pagination={{ clickable: true }}
-        modules={[Pagination, Navigation]}
-        className="relative h-60"
-      >
-        {movies.map((movie) => (
-          <SwiperSlide key={movie.id}>
-            <MovieCard movie={movie} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+          <div className="absolute -top-3 right-0 w-1/6 h-1 bg-gray-500">
+            <div
+              className="h-full bg-white transition-all duration-300"
+              style={{
+                width: `${((activeIndex + 1) / movies.length) * 100}%`,
+              }}
+            ></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
 const Body = (moviesData) => {
-  console.log(moviesData);
   return (
-    <div className="px-8 pb-10 bg-[#141414] text-white">
+    <div className="bg-[#141414] text-white w-[100vw]">
       {Object.entries(moviesData).map(([category, movies], index) => (
         <Section
           key={category}
@@ -61,47 +87,3 @@ const Body = (moviesData) => {
 };
 
 export default Body;
-
-// const Section = ({ title, movies, index }) => {
-//   return (
-//     <div className="relative">
-//       <h1 className="pb-4 pl-10 text-xl">{title}</h1>
-
-//       <button className={`custom-prev-${index} absolute -left-8 top-1/2 transform bg-black text-white p-2 z-10 rounded-full `}>
-//         <ChevronLeft size={24} />
-//       </button>
-//       <button className={`custom-next-${index} absolute -right-8 top-1/2 transform bg-black text-white p-2 z-10 rounded-full`}>
-//         <ChevronRight size={24} />
-//       </button>
-
-//       <Swiper
-//         slidesPerView={4}
-//         slidesPerGroup={1}
-//         loop={true}
-//         navigation={{
-//           nextEl: `.custom-next-${index}`,
-//           prevEl: `.custom-prev-${index}`,
-//         }}
-//         pagination={{ clickable: true }}
-//         modules={[Pagination, Navigation]}
-//         className="relative h-60"
-//       >
-//         {movies.map((movie, index) => (
-//           <SwiperSlide key={index} className="">
-//             <div
-//               key={movie.id}
-//               className="h-[80%] relative m-4 hover:transform hover:scale-105 transition-transform duration-300"
-//               style={{
-//                 backgroundImage: `url(https://image.tmdb.org/t/p/w500/${movie.backdrop_path})`,
-//               }}
-//             >
-//               <h3 className="text-white text-lg font-semibold absolute bottom-0 left-2">
-//                 {movie.title || movie.name}
-//               </h3>
-//             </div>
-//           </SwiperSlide>
-//         ))}
-//       </Swiper>
-//     </div>
-//   );
-// };
